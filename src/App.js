a003c668ea74d0d4c992d92aba62bbfb18cd0d9d
Tarteel420 <tarteelimran@gmail.com>
2024-01-18 14:49:33 +0500

    uploaded files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Register from './Register'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <main className="App">
+      <Register/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+//css file
+import 'bootstrap/dist/css/bootstrap.min.css'
+//js file  
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
